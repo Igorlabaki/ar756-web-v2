25fdfc5f87a84dc29c64bbaa600ba32b38e586b1
Igor Goncalo <igorlabakig@gmail.com>
2024-01-22 18:26:59 +0000

    f

--- src/components/welcome/videobg.tsx
+++ src/components/welcome/videobg.tsx
@@ -1,8 +1,7 @@
-
 export  function VideobgComponent() {
   return (
     <div className="w-full h-full object-cover">
-     <video className="max-h-full min-h-screen min-w-screen object-cover" src={"/assets/video/videoBg.mp4"} autoPlay loop muted/>
+     <video className="max-h-full min-h-screen min-w-screen object-cover" src={"../../assets/video/videoBg.mp4"} autoPlay loop muted/>
     </div>
   );
 }

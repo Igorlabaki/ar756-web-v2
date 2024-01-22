68b1fa57ce87575059544c43514a7d055d3fd7de
Igor Goncalo <igorlabakig@gmail.com>
2024-01-22 18:11:26 +0000

    first commit

---
+++ src/components/welcome/videobg.tsx
@@ -0,0 +1,8 @@
+
+export  function VideobgComponent() {
+  return (
+    <div className="w-full h-full object-cover">
+     <video className="max-h-full min-h-screen min-w-screen object-cover" src={"../../assets/video/videoBg.mp4"} autoPlay loop muted/>
+    </div>
+  );
+}
